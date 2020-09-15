73b921aba807af4bbbf76d735b7eba0f47dc04b8
Manish Tuladhar <manish.eclipse@gmail.com>
2020-09-15 19:01:10 +0545

    Created Use Case abstract class and implemented random trivia use case

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -1,6 +1,4 @@
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
 import '../../../../core/usecases/usecase.dart';
 import '../../../../core/error/failures.dart';
 import '../entities/number_trivia.dart';
@@ -8,27 +6,16 @@ import '../repositories/number_trivia_repositories.dart';
 
 /// GetConcreteNumberTrivia Class: It is an use case class that handles getting
 /// data from the network or repository
-class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
   final NumberTriviaRepository repository;
 
-  GetConcreteNumberTrivia(this.repository);
+  GetRandomNumberTrivia(this.repository);
 
   /// The return value may either be failure or value: as Either<L,R>
   /// The call methods helps to directly call a class function
   /// rather than pointing to specific part
   @override
-  Future<Either<Failure, NumberTrivia>> call(Params params) async {
-    return await repository.getConcreteNumberTrivia(params.number);
+  Future<Either<Failure, NumberTrivia>> call(NoParams noParams) async {
+    return await repository.getRandomNumberTrivia();
   }
-}
-
-/// Creating a params class so that it will
-/// help us pass values back and forth the main use case
-class Params extends Equatable {
-  final int number;
-
-  Params({@required this.number});
-
-  @override
-  List<Object> get props => [number];
 }
--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -1,9 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/usecases/usecase.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repositories.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 
 /// Since we have the contract/definition of the repository it is far
 /// easier to do the testing with TDD approach for the use cases with need of implementation
@@ -14,38 +16,37 @@ class MockNumberTriviaRepository extends Mock
 /// Here we run all our tests
 void main() {
   // Values needed : UseCase and MockClass
-  GetConcreteNumberTrivia usecase;
+  GetRandomNumberTrivia usecase;
   MockNumberTriviaRepository mockNumberTriviaRepository;
 
   /// This thing setups the necessary inits before the test
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
-    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
   });
 
   /// Starting for the test
   /// initial values for passing
-  final tNumber = 1;
   final tNumberTrivia = NumberTrivia(number: 1, text: 'test');
 
   /// Test should return back values needed from the repo
   /// Here, it should return any Right value so that repo returns data
-  test('should get number trivia from repository', () async {
+  test('should get trivia from repository', () async {
     // ARRANGE
     // make sure that the mock class function returns any data when
     // called that must of Right type i.e. No failure
-    when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
+    when(mockNumberTriviaRepository.getRandomNumberTrivia())
         .thenAnswer((_) async => Right(tNumberTrivia));
 
     // ACT
     // The "act" phase of the test. Call the not-yet-existent method.
-    final result = await usecase(Params(number: tNumber));
+    final result = await usecase(NoParams());
 
     // ASSERT
     // UseCase should simply return whatever was returned from the Repository
     expect(result, Right(tNumberTrivia));
     // Verify that the method has been called on the Repository
-    verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+    verify(mockNumberTriviaRepository.getRandomNumberTrivia());
     // Verify that the method has been called on the Repository
     verifyNoMoreInteractions(mockNumberTriviaRepository);
   });
